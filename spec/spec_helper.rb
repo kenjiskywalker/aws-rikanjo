192151f1479ad646f7687c5f79b8af6e46f52c78
toritori0318 <toritori0318@gmail.com>
2014-05-14 02:50:23 +0900

    cleanup test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,25 @@ end
 
 RSpec.configure do |config|
 end
+
+module RikanjoSpecHelper
+  def regions
+    return %w{
+      eu-west-1
+      sa-east-1
+      us-east-1
+      ap-northeast-1
+      us-west-2
+      us-west-1
+      ap-southeast-1
+      ap-southeast-2
+    }
+  end
+
+  def get_sleep url
+    contents = Net::HTTP.get(URI.parse(url))
+    sleep 1
+    return contents
+  end
+
+end
